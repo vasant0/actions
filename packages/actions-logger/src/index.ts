55ff94fb6f889285954d01607cfea712e8f2b8e4
svaj <chris@svajlenka.com>
2020-05-13 13:55:02 -0400

    fix(logger): fixed ts stuff in action logger

--- packages/actions-logger/src/index.ts
+++ packages/actions-logger/src/index.ts
@@ -1,1 +1,52 @@
-export * from './logger'
+import {warning, info, debug, error} from '@actions/core'
+import {logMethod, ActionLogger} from './types'
+
+export const TAB_WIDTH = 2
+
+export const niceLog = (level: string, message = '', ...params: object[]): void => {
+  let fn
+  switch (level) {
+    default:
+    case 'log':
+    case 'info':
+      fn = info
+      break
+    case 'debug':
+      fn = debug
+      break
+    case 'error':
+      fn = error
+      break
+    case 'warn':
+    case 'warning':
+      fn = warning
+      break
+  }
+  fn(JSON.stringify({message, details: {...params}}, null, TAB_WIDTH))
+}
+export const niceLogForLevel = (level: string): logMethod => (message = '', ...params: object[]): void =>
+  niceLog(level, message, ...params)
+
+export const DEFAULT_LEVELS = ['info', 'log', 'debug', 'warn', 'warning', 'error']
+
+export const ALLOWED_LEVELS = [...DEFAULT_LEVELS]
+
+export const Logger = (levels: string[] = DEFAULT_LEVELS): ActionLogger =>
+  levels
+    .filter(level => level in ALLOWED_LEVELS)
+    .reduce(
+      (logger, level): ActionLogger => ({
+        ...logger,
+        [level]: niceLogForLevel(level),
+      }),
+      {},
+    )
+
+export const logger = Logger()
+
+export default {
+  Logger,
+  DEFAULT_LEVELS,
+  niceLog,
+  logger,
+}
